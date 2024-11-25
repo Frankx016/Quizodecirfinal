21d27d95a78a173113be800951b7bb6f25745c0a
Frankx016 <sandovaljfrancisco62@gmail.com>
2024-11-25 12:57:43 -0600

    Add files via upload
    Lista de palabras a sugerir

---
+++ listafinal.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+void main(void)
+{
+    
+    int iNumLista = 1;	//Una sola palabra candidata
+    int sugeridas = 0;
+    int elementos;
+    char sugeridas;
+    char palabras;
+    char lista;
+    int peso;
+    int estadistica;
+    for (i = 0; i < sugeridas; i++)
+    {
+        for (j = 0; j < elementos; j++)
+        {
+            if (strcmp(sugeridas[i], palabras[j]) == 0)
+            {
+
+                strcpy_s(lista[iNumLista]);
+                Peso[iNumLista] = Estadistica[j];
+                iNumLista++;
+            }
+        }
+    }
+}
